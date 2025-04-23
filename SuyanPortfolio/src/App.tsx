e9a68d0c3631aa4fecdfdde556facffcc0ace77d
suyanthapa <146444078+suyanthapa@users.noreply.github.com>
2025-04-23 17:31:57 +0545

    added

--- SuyanPortfolio/src/App.tsx
+++ SuyanPortfolio/src/App.tsx
@@ -3,7 +3,7 @@ import Contact from './components/contact';
 import Home from './components/home';
 import MyJourney from './components/myJourney';
 import Navbar from './components/Navbar'; // Import the Navbar component
-import Skills from './components/skills';
+import Skills from './components/Skills';
 
 const App = () => {
   return (
